03615569954599f1234c23e8d3f4908251ce7412
ivan <tuchel.ivan@gmail.com>
2022-01-27 16:52:16 +0300

    initialize App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,25 @@
 import React from 'react';
+import { Container, Stack, Button } from 'react-bootstrap';
+import BudgetCard from './components/BudgetCard';
 
-const App = () => <div className="App">Budget Tracker</div>;
+const App = () => (
+  <Container className="my-4">
+    <Stack direction="horizontal" gap="2" className="mb-4">
+      <h1 className="me-auto">Budget</h1>
+      <Button variant="primary">Add Budget</Button>
+      <Button variant="outline-primary">Add Expense</Button>
+    </Stack>
+    <div
+      style={{
+        display: 'grid',
+        gridTemplateColumns: 'repeat(auto-fill, minmax(320px, 1fr))',
+        gap: '1rem',
+        alignItems: 'flex-start',
+      }}
+    >
+      <BudgetCard name="Entertainment" amount={200} max={1000} />
+    </div>
+  </Container>
+);
 
 export default App;
