723ddf4a01009c090f12e68a74fe588d608ccd80
ivan <tuchel.ivan@gmail.com>
2022-02-08 16:14:55 +0300

    create AddBudgetModal & refactor App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,50 @@
 import React from 'react';
 import { Container, Stack, Button } from 'react-bootstrap';
-import BudgetCard from './components/BudgetCard';
 
-const App = () => (
-  <Container className="my-4">
-    <Stack direction="horizontal" gap="2" className="mb-4">
-      <h1 className="me-auto">Budget</h1>
-      <Button variant="primary">Add Budget</Button>
-      <Button variant="outline-primary">Add Expense</Button>
-    </Stack>
-    <div
-      style={{
-        display: 'grid',
-        gridTemplateColumns: 'repeat(auto-fill, minmax(320px, 1fr))',
-        gap: '1rem',
-        alignItems: 'flex-start',
-      }}
-    >
-      <BudgetCard name="Entertainment" gray amount={80} max={800} />
-    </div>
-  </Container>
-);
+import BudgetCard from './components/BudgetCard';
+import AddBudgetModal from './components/Modals/AddBudgetModal';
+
+import useBudgets from './hooks/useBudgets';
+
+const App = () => {
+  const [showAddBudgetModal, setShowAddBudgetModal] = React.useState(false);
+  const { budgets, getBudgetExpenses } = useBudgets();
+
+  return (
+    <>
+      <Container className="my-4">
+        <Stack direction="horizontal" gap="2" className="mb-4">
+          <h1 className="me-auto">Budget</h1>
+          <Button variant="primary" onClick={() => setShowAddBudgetModal(true)}>
+            Add Budget
+          </Button>
+          <Button variant="outline-primary">Add Expense</Button>
+        </Stack>
+        <div
+          style={{
+            display: 'grid',
+            gridTemplateColumns: 'repeat(auto-fill, minmax(320px, 1fr))',
+            gap: '1rem',
+            alignItems: 'flex-start',
+          }}
+        >
+          {budgets.map(({ id, name, max }) => {
+            const calculatedAmount = getBudgetExpenses(id).reduce(
+              (total, expense) => total + expense.amount,
+              0,
+            );
+            return (
+              <BudgetCard key={id} name={name} gray amount={calculatedAmount} max={max} />
+            );
+          })}
+        </div>
+      </Container>
+      <AddBudgetModal
+        show={showAddBudgetModal}
+        handleClose={() => setShowAddBudgetModal(false)}
+      />
+    </>
+  );
+};
 
 export default App;
