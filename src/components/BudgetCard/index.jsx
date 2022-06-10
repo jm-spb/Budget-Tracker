64b88e77f7c0162b3797f1134e58bfd929789ce9
ivan <tuchel.ivan@gmail.com>
2022-06-10 22:11:33 +0300

    add utils, conditional render, onClick functions

--- src/components/BudgetCard/index.jsx
+++ src/components/BudgetCard/index.jsx
@@ -2,17 +2,9 @@ import React from 'react';
 import { Card, ProgressBar, Stack, Button } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
+import { currencyFormatter, getProgressBarVariant } from '../../utils';
 
-import currencyFormatter from '../../utils';
-
-const getProgressBarVariant = (amount, max) => {
-  const ratio = amount / max;
-  if (ratio < 0.5) return 'primary';
-  if (ratio < 0.75) return 'warning';
-  return 'danger';
-};
-
-const BudgetCard = ({ name, amount, max, gray, onAddExpenseClick }) => {
+const BudgetCard = ({ name, amount, max, gray, onAddExpenseClick, onViewExpensesClick, hideButtons }) => {
   const cardClassName = classNames({
     'bg-light': gray && amount <= max,
     'bg-danger bg-opacity-10': amount > max,
@@ -25,14 +17,14 @@ const BudgetCard = ({ name, amount, max, gray, onAddExpenseClick }) => {
           <div className="me-2">{name}</div>
           <div className="d-flex align-items-baseline">
             {currencyFormatter.format(amount)}
-            {max && (
+            {max !== Infinity && (
               <span className="text-muted fs-6 ms-1">
                 / {currencyFormatter.format(max)}
               </span>
             )}
           </div>
         </Card.Title>
-        {max && (
+        {max !== Infinity && (
           <ProgressBar
             className="rounded-pill"
             variant={getProgressBarVariant(amount, max)}
@@ -41,16 +33,18 @@ const BudgetCard = ({ name, amount, max, gray, onAddExpenseClick }) => {
             now={amount}
           />
         )}
-        <Stack direction="horizontal" gap="2" className="mt-4">
-          <Button
-            variant="outline-primary"
-            className="ms-auto"
-            onClick={onAddExpenseClick}
-          >
-            Add Expense
-          </Button>
-          <Button variant="outline-secondary">View Expense</Button>
-        </Stack>
+        {!hideButtons && (
+          <Stack direction="horizontal" gap="2" className="mt-4">
+            <Button
+              variant="outline-primary"
+              className="ms-auto"
+              onClick={onAddExpenseClick}
+            >
+              Add Expense
+            </Button>
+            <Button onClick={onViewExpensesClick} variant="outline-secondary">View Expense</Button>
+          </Stack>
+        )}
       </Card.Body>
     </Card>
   );
@@ -61,7 +55,16 @@ export default BudgetCard;
 BudgetCard.propTypes = {
   name: PropTypes.string.isRequired,
   amount: PropTypes.number.isRequired,
-  max: PropTypes.number.isRequired,
+  max: PropTypes.number,
   gray: PropTypes.bool.isRequired,
-  onAddExpenseClick: PropTypes.func.isRequired,
+  hideButtons: PropTypes.bool,
+  onAddExpenseClick: PropTypes.func,
+  onViewExpensesClick: PropTypes.func,
+};
+
+BudgetCard.defaultProps = {
+  max: Infinity,
+  hideButtons: false,
+  onAddExpenseClick: () => {},
+  onViewExpensesClick: () => {},
 };
