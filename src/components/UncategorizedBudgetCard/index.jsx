61fbac8a8c63a3973f690658fb755626ee4ced88
ivan <tuchel.ivan@gmail.com>
2022-06-10 22:14:01 +0300

    refactor component

--- src/components/UncategorizedBudgetCard/index.jsx
+++ src/components/UncategorizedBudgetCard/index.jsx
@@ -1,19 +1,31 @@
 import React from 'react';
-
+import PropTypes from 'prop-types';
 import BudgetCard from '../BudgetCard';
-
 import UNCATEGORIZED_BUDGET_ID from '../../constants';
 import useBudgets from '../../hooks/useBudgets';
+import { calculateValueSum } from '../../utils';
 
-const UncategorizedBudgetCard = () => {
+const UncategorizedBudgetCard = ({ onAddExpenseClick, onViewExpensesClick }) => {
   const { getBudgetExpenses } = useBudgets();
-  const calculatedAmount = getBudgetExpenses(UNCATEGORIZED_BUDGET_ID).reduce(
-    (total, expense) => total + expense.amount,
-    0,
-  );
+  const uncatExpenses = getBudgetExpenses(UNCATEGORIZED_BUDGET_ID);
+  const calculatedAmount = calculateValueSum(uncatExpenses, 'amount');
+
   if (calculatedAmount === 0) return null;
 
-  return <BudgetCard amount={calculatedAmount} name="Uncategorized" gray />;
+  return (
+    <BudgetCard
+      amount={calculatedAmount}
+      name="Uncategorized"
+      gray
+      onAddExpenseClick={onAddExpenseClick}
+      onViewExpensesClick={onViewExpensesClick}
+    />
+  );
 };
 
-export default UncategorizedBudgetCard;
+export default React.memo(UncategorizedBudgetCard);
+
+UncategorizedBudgetCard.propTypes = {
+  onAddExpenseClick: PropTypes.func.isRequired,
+  onViewExpensesClick: PropTypes.func.isRequired,
+};
